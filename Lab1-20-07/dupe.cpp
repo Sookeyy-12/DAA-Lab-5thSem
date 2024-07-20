b1ea009d7d90097b60da3550280e73bf5b64241e
Sookeyy-12 <thatonesuket@gmail.com>
2024-07-20 10:31:04 +0530

    lab1

--- Lab1-20-07/dupe.cpp
+++ Lab1-20-07/dupe.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void solve(vector<int>& numbers) {
+    // print the read elements
     cout << "Content of the array: " << endl;
     for (auto n: numbers) cout << n << " ";
     cout << endl;
+    // create a frequency map
     unordered_map<int, int> mp;
     for (auto n: numbers) {
         mp[n]++;
@@ -13,9 +15,11 @@ void solve(vector<int>& numbers) {
     int freq = 0;
     int cnt =0 ;
     for (auto m: mp) {
+        // count the elements with frequency more than 1
         if (m.second > 1) {
             cnt++;
         }
+        // check freq of current most freq element and update
         if (m.second > freq) {
             freqel = m.first;
             freq = m.second;
@@ -29,6 +33,7 @@ int main() {
     int n; 
     cout << "Enter how many numbers you want to read from file? \n";
     cin >> n;
+    // read input from text file
     ifstream inputFile("q5input.txt");
     vector<int> numbers(n);
     for (int i = 0; i < n; i++) {
