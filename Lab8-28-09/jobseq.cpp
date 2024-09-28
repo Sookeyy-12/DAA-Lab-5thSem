a29c750c091ce7eb5e831653147023f07248a829
Sookeyy-12 <thatonesuket@gmail.com>
2024-09-28 10:23:08 +0530

    jobseq

--- Lab8-28-09/jobseq.cpp
+++ Lab8-28-09/jobseq.cpp
@@ -1,10 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    
+void solve(vector<pair<int, pair<int, int>>> &prof_job_dead, int n)
+{
+    sort(prof_job_dead.rbegin(), prof_job_dead.rend());
+    int result[n];
+    bool slot[n];
+    for (int i = 0; i < n; i++)
+        slot[i] = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = min(n, prof_job_dead[i].second.second) - 1; j >= 0; j--)
+        {
+            // Free slot found
+            if (slot[j] == false)
+            {
+                result[j] = i;
+                slot[j] = true;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+        if (slot[i])
+            cout << prof_job_dead[result[i]].second.first << " ";
 }
 
-int main() {
-    
+int main()
+{
+    int n;
+    cin >> n;
+    // job id, profit, deadline
+    vector<pair<int, pair<int, int>>> prof_job_dead(n);
+    for (int i = 0; i < n; i++)
+    {
+        int job;
+        cin >> job;
+        int prof;
+        int dead;
+        cin >> dead;
+        cin >> prof;
+        prof_job_dead[i] = {prof, {job, dead}};
+    }
+    solve(prof_job_dead, n);
 }
--- README.md
+++ README.md
@@ -37,6 +37,6 @@ All the programs done in Design and Analysis of Algorithms Lab in my 5th Semeste
     - Non-increasing order of profit/weight ratio
 
 ## Lab 8 | 28-09-2024
-1. Job Sequencing
+1. `jobseq.cpp` Implement Job Sequencing Algorithm.
 2. `prims.cpp` Implement Prims Algorithm to find Minimum Spanning Tree and its sum.
-3. Kruskals
+3. `kruskals.cpp` Implement Kruskal's Algorithm to find Minimum Spanning Tree and its sum.
